66d0d0d1fe8af131907d82538454a3e28fd2c73b
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2022-06-03 07:17:01 +0700

    Загруженность

--- io4.py
+++ io4.py
@@ -28,16 +28,24 @@ def addObrabotka(a, b, c):
         plt.plot([a, (b + a) / 2], [-2, -1], color='red')
         plt.plot([(b + a) / 2, b], [-1, -2], color='red')
 
+def addState(a, b, c):
+    if c == 0:
+        pass
+    elif c == 1:
+        plt.plot([a, b], [-3, -3], color='green')
+    elif c == 2:
+        plt.plot([a, b], [-3, -3], color='yellow')
+    elif c == 3:
+        plt.plot([a, b], [-3, -3], color='red')
 
 # генерируем челов
 stream = [[0, True]]
 for i in range(12):
     stream.append([stream[len(stream) - 1][0] + rand(l, k), True])
 
-# queue.append(1)
-# del  queue[0]
 obrabotka = [[0, stream[1][0]]]  # при отрисовки делитнуть первый элемент
 queue = [stream[1]]  # первый чел в очереди это чел, которого мы обрабатываем
+state = []
 si = 1
 while True:
     # проверка начинать с предыдущего интервала или ждать следующего
@@ -56,19 +64,25 @@ while True:
                 stream[si + 1][1] = False
             else:
                 queue.append(stream[si + 1])
+                state.append([stream[si + 1][0], len(queue)])
             si += 1
         else:
             break
+    state.append([obrabotka[len(obrabotka) - 1][1], len(queue)])
     if si > len(stream) - 2:
         # print("queue", len(queue))
         break
     if len(queue) <= 0:  # если в очереди никого нет, добавляем в обработку некст чела
         si += 1
+        state.append([stream[si + 1][0], len(queue)])
         queue.append(stream[si])
 # Если остались челы в очереди, то их дообработать
+j = 0
 for i in queue:
+    j += 1
     obrabotka.append([obrabotka[len(obrabotka) - 1][1],
                       obrabotka[len(obrabotka) - 1][1] + random.expovariate(mu)])  # обрабатываем текущего чела
+    state.append([obrabotka[len(obrabotka) - 1][1], len(queue) - j])
 queue = []
 
 
@@ -77,8 +91,8 @@ kolSuccess = 0
 for i in stream:
     if i[1]:
         kolSuccess += 1
-print("Относительная пропускная способность: ", (kolSuccess-1) / (len(stream)-1))
-print("Абсолютная пропускная способность", (len(obrabotka)-1)/(obrabotka[len(obrabotka)-1][1] - obrabotka[0][1]))
+print("Относительная пропускная способность:\t", (kolSuccess-1) / (len(stream)-1))
+print("Абсолютная пропускная способность:\t", (len(obrabotka)-1)/(obrabotka[len(obrabotka)-1][1] - obrabotka[0][1]))
 
 
 # Отрисовка
@@ -88,6 +102,16 @@ for i in range(1, len(stream)):  # рисуем stream
 del obrabotka[0]
 for i in range(len(obrabotka)):  # рисуем stream
     addObrabotka(obrabotka[i][0], obrabotka[i][1], True)
+
+flag = True
+while flag:
+    flag = False
+    for i in range(len(state)-1):
+        if state[i][0] > state[i+1][0]:
+            state[i], state[i+1] = state[i+1], state[i]
+            flag = True
+for i in range(1, len(state)):
+    addState(state[i-1][0], state[i][0], state[i-1][1])
 
 plt.ylim([-10, 10])
 matplotlib.pyplot.show()
